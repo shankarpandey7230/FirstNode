b7330abeadd6b70d8225186a9e3437b476abeb1f
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-09-30 05:18:19 +1000

    method post Javascript Example

--- app.js
+++ app.js
@@ -2,23 +2,36 @@ const express = require("express");
 const app = express();
 let { people } = require("./data");
 
-//static assets
-
+// static assets
 app.use(express.static("./methods-public"));
 // parse form data
 app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
+
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
 
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .send({ success: false, msg: "please provide name value" });
+  }
+  res.status(201).send({ success: true, person: name });
+});
+
 app.post("/login", (req, res) => {
-  // console.log(req.body);
   const { name } = req.body;
   if (name) {
     return res.status(200).send(`Welcome ${name}`);
   }
-  res.status(401).send("Please Provide credentials");
+
+  res.status(401).send("Please Provide Credentials");
 });
+
 app.listen(3000, () => {
-  console.log("Server is listening on port 3000..");
+  console.log("Server is listening on port 5000....");
 });
