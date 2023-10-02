e442bd6b720db57090805fd36a2a7d61b335eba9
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-10-02 20:18:25 +1100

    setting up express router

--- routes/people.js
+++ routes/people.js
@@ -1,27 +1,13 @@
 const express = require("express");
-const app = express();
-let { people } = require("./data");
 
-// static assets
-app.use(express.static("./methods-public"));
-// parse form data
-app.use(express.urlencoded({ extended: false }));
-// parse json
-app.use(express.json());
-app.post("/login", (req, res) => {
-  const { name } = req.body;
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`);
-  }
+const router = express.Router();
+let { people } = require("../data");
 
-  res.status(401).send("Please Provide Credentials");
-});
-
-app.get("/api/people", (req, res) => {
+router.get("/", (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
 
-app.post("/api/people", (req, res) => {
+router.post("/", (req, res) => {
   const { name } = req.body;
   if (!name) {
     return res
@@ -30,7 +16,7 @@ app.post("/api/people", (req, res) => {
   }
   res.status(201).send({ success: true, person: name });
 });
-app.post("/api/people/postman", (req, res) => {
+router.post("/postman", (req, res) => {
   const { name } = req.body;
   if (!name) {
     return res
@@ -40,7 +26,7 @@ app.post("/api/people/postman", (req, res) => {
   res.status(201).json({ success: true, data: [...people, name] });
 });
 
-app.put("/api/people/:id", (req, res) => {
+router.put("/:id", (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
 
@@ -59,7 +45,7 @@ app.put("/api/people/:id", (req, res) => {
   });
   res.status(200).json({ success: true, data: newPeople });
 });
-app.delete("/api/people/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
   const person = people.find((person) => person.id === Number(req.params.id));
   if (!person) {
     return res
@@ -71,6 +57,5 @@ app.delete("/api/people/:id", (req, res) => {
   );
   return res.status(200).json({ success: true, data: newPeople });
 });
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000....");
-});
+
+module.exports = router;
